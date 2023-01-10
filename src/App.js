416ae7ff72d4bb3100dd5e3967b6abbd1febd868
Aziat <abdimalikovaziat@gmail.com>
2023-01-10 12:01:16 +0600

    add dummy-post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import PostList from "./components/postList/PostList";
+
+const DUMMY_POSTS = [
+  {
+    id: 1,
+    title: "JavaScript",
+    body: "Description",
+  },
+  {
+    id: 2,
+    title: "React",
+    body: "Description 2",
+  },
+  {
+    id: 3,
+    title: "Next.js",
+    body: "Description 3",
+  },
+];
 
 function App() {
-  return <main className="App">
-    lifting-up
-  </main>;
+  const [posts, setPosts] = useState(DUMMY_POSTS);
+
+  return (
+    <div className="App">
+    <PostList />
+    </div>
+  );
 }
 
 export default App;
